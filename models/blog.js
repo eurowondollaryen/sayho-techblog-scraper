9c43e185f2f36c778ea87b4ca1a7252bb558cd54
eurowondollaryen <sehoakasayho@naver.com>
2020-12-09 08:25:58 +0900

    post count

--- models/blog.js
+++ models/blog.js
@@ -8,7 +8,7 @@ TODO : 현재는 하드코딩되어있지만, DB에서 가져오기
 const pool = require('../db.js').pool;
 exports.getBlogList = async function() {
 	var result = [];
-	await pool.query("SELECT * FROM ICTBLOGS")
+	await pool.query("SELECT BLOG_ID, TITLE_KR, TITLE_EN, ROUTE, BASE_URL, (SELECT COUNT(*) FROM ICTPOSTS X WHERE X.BLOG_ID = A.BLOG_ID) CNT FROM ICTBLOGS A")
 	.then(function(res) { result = res.rows; });
 	return result;
 }
