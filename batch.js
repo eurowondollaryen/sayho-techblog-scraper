2d202cb95dc25793e2fcf523bab6f3b8357b25cc
eurowondollaryen <sehoakasayho@naver.com>
2020-12-09 13:00:03 +0900

    batch writing

--- batch.js
+++ batch.js
@@ -26,34 +26,54 @@ var scraping = async function() {
 		console.log("getting initial data...");
 		await initGlobal();//초기데이터 가져오는데 성공!
 		console.log(global_urls);
-		console.log("batch start!!");
+		console.log("batch start!! - " + new Date().toString());
 		
-		//2. naver
-		(async function example() {
-			var data = [];
-			let driver = await new Builder().forBrowser('chrome').build();
-			try {
-				// Navigate to Url
-				await driver.get(global_urls[1]["base_url"]);
-				//wait till loaded
-				await driver.wait(until.elementLocated(By.css('div.contents>div.post_article>div.cont_post')), 10000);
+		for(var i = 0; i < global_urls.length; ++i) {
+			var blog_id = global_urls[i]["blog_id"];
+			if(blog_id == "1001") {
 				
-				let elements = await driver.findElements(By.css('div.contents>div.post_article>div.cont_post'));
-				let count = 0;
-				
-				for(let e of elements) {
-					data.push({});
-					data[count]["url"] = await e.findElement(By.css("h2>a")).getAttribute("href");
-					data[count]["title"] = await e.findElement(By.css("h2>a")).getText();
-					data[count++]["subtitle"] = await e.findElement(By.css("a.post_txt_wrap>div.post_txt")).getText();
-				}
+			} else if (blog_id == "1002") {
+				//2. naver
+				(async function example() {
+					var data = [];
+					let driver = await new Builder().forBrowser('chrome').build();
+					try {
+						// Navigate to Url
+						await driver.get(global_urls[1]["base_url"]);
+						//wait till loaded
+						await driver.wait(until.elementLocated(By.css('div.contents>div.post_article>div.cont_post')), 10000);
+						
+						let elements = await driver.findElements(By.css('div.contents>div.post_article>div.cont_post'));
+						let count = 0;
+						
+						for(let e of elements) {
+							data.push({});
+							data[count]["url"] = await e.findElement(By.css("h2>a")).getAttribute("href");
+							data[count]["title"] = await e.findElement(By.css("h2>a")).getText();
+							data[count++]["subtitle"] = await e.findElement(By.css("a.post_txt_wrap>div.post_txt")).getText();
+						}
+					}
+					finally{
+						console.log(data);
+						
+						driver.quit();
+						//TODO : SELENIUM으로 조회된 OBJECT 배열을 DB에서 조회해온 내용과 비교하여, UPDATE OR INSERT
+					}
+				})();
+			} else if (blog_id == "1003") {
+			} else if (blog_id == "1004") {
+			} else if (blog_id == "1005") {
+			} else if (blog_id == "1006") {
+			} else if (blog_id == "1007") {
+			} else if (blog_id == "1008") {
+			} else if (blog_id == "1009") {
+			} else if (blog_id == "1010") {
+			} else if (blog_id == "1011") {
+			} else {
+				console.log("[Error] batch logic Not written!");
 			}
-			finally{
-				console.log(data);
-				driver.quit();
-				//TODO : SELENIUM으로 조회된 OBJECT 배열을 DB에서 조회해온 내용과 비교하여, UPDATE OR INSERT
-			}
-		})();
+		}
+		
 };
 
 
