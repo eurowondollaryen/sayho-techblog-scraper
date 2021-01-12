d9186bc66e2371fd3d7b13e240cda2db80c2adf0
eurowondollaryen <sehoakasayho@naver.com>
2021-01-12 12:38:57 +0900

    admin password page added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,15 @@
-var query = require("../models/admin.js");
+var admin = require("../models/admin.js");
 
 //admin page
 const adminIndex = async function(req, res) {
-	res.render("adminIndex", {});
+	res.render("adminPassword", {});
 }
 //export controller functions
 exports.adminIndex = adminIndex;
+/*
+TODO : ADMIN
+1. [HOST]/admin에 접속한다.
+1-a. 세션이 있으면 admin 메뉴페이지를 보여준다.
+1-b. 세션이 없으면 비밀번호 입력 페이지를 보여준다.
+2. 비밀번호 입력 후 일치하면, 세션 생성하고, admin 메뉴페이지로 이동한다.
+*/
