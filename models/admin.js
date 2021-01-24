0eb8b00ec0ac4857be7bee9280c281914a86fcbd
eurowondollaryen <sehoakasayho@naver.com>
2021-01-24 23:06:07 +0900

    added viewstats done

--- models/admin.js
+++ models/admin.js
@@ -30,7 +30,7 @@ exports.statVisit = async function() {
 exports.statViewCount = async function() {
 	var list = [];
 	await pool.query(`
-	SELECT A.CNT CNT, A.POST_SEQ POST_SEQ, B.BLOG_ID BLOG_ID, B.TITLE TITLE, B.POST_URL POST_URL
+	SELECT ROW_NUMBER() OVER(ORDER BY A.CNT DESC, A.POST_SEQ ASC) ROWNUM, (SELECT TITLE_KR FROM ICTBLOGS X WHERE X.BLOG_ID = B.BLOG_ID) BLOG_NAME, B.TITLE TITLE, B.POST_URL POST_URL, A.CNT CNT
 	FROM (SELECT COUNT(1) CNT, POST_SEQ
 			FROM ICTVIEWLOG
 			GROUP BY POST_SEQ
@@ -38,6 +38,7 @@ exports.statViewCount = async function() {
 			(SELECT BLOG_ID, POST_SEQ, TITLE, POST_URL
 			FROM ICTPOSTS) B
 	ON A.POST_SEQ = B.POST_SEQ
+	WHERE A.POST_SEQ IS NOT NULL
 	ORDER BY A.CNT DESC, A.POST_SEQ ASC
 	`).then(function(res) { list = res.rows; });
 	return list;
