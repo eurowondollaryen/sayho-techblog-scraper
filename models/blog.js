558749cffeac377638bf9cc1098c672f7072b0d7
eurowondollaryen <sehoakasayho@naver.com>
2021-02-22 19:43:28 +0900

    added log

--- models/blog.js
+++ models/blog.js
@@ -8,6 +8,7 @@ TODO : 현재는 하드코딩되어있지만, DB에서 가져오기
 const pool = require('../db.js').pool;
 exports.getBlogList = async function() {
 	var result = [];
+	console.log("[blog.js][getBlogList] execute query");
 	await pool.query("SELECT BLOG_ID, TITLE_KR, TITLE_EN, ROUTE, BASE_URL"
 	+ ", (SELECT COUNT(*) FROM ICTPOSTS X WHERE X.BLOG_ID = A.BLOG_ID) CNT"
 	+ ", (SELECT CASE WHEN MAX(INST_DT) = TO_CHAR(NOW(),'YYYYMMDD') THEN '1' ELSE '0' END FROM ICTPOSTS X WHERE X.BLOG_ID = A.BLOG_ID) NEWPOST"
@@ -18,6 +19,7 @@ exports.getBlogList = async function() {
 
 exports.getBlogId = async function(parameters) {
 	var result = [];
+	console.log("[blog.js][getBlogId] execute query");
 	await pool.query("SELECT TITLE_KR, TITLE_EN, BASE_URL FROM ICTBLOGS WHERE ROUTE = $1", parameters)
 	.then(function(res) { result = res.rows; } );
 	return result;
