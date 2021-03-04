bf66ff33e3c4f2b6a2575a6665f0b4e5df56a449
eurowondollaryen <sehoakasayho@naver.com>
2021-03-04 23:02:42 +0900

    get rid of req.param

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,53 +1,57 @@
+//post : req.body, get(url) : req.query
 var post = require("../models/post.js");
 var blog = require("../models/blog.js");
 
-const list = function(req, res) {
-	console.log("list");
+const list = function (req, res) {
+  console.log("list");
 };
 
-const create = function(req, res) {
-	console.log("create");
+const create = function (req, res) {
+  console.log("create");
 };
 
-const update = function(req, res) {
-	console.log("update");
+const update = function (req, res) {
+  console.log("update");
 };
 
-const remove = function(req, res) {
-	console.log("remove");
+const remove = function (req, res) {
+  console.log("remove");
 };
 
-const getPostListGet = async function(req, res) {
-	console.log("[postController][getPostList]request data : " + req.param("company"));
-	var parameters = [req.param("company")];
-	//parameter가 null이면 404
-	if(parameters[0] == null || parameters[0] == undefined) {
-		res.end('<head><title>404</title></head><body><h1>404 Error!</h1></body>');
-		return;
-	}
+const getPostListGet = async function (req, res) {
+  console.log(
+    "[postController][getPostList]request data : " + req.query["company"]
+  );
+  var parameters = [req.query["company"]];
+  //parameter가 null이면 404
+  if (parameters[0] == null || parameters[0] == undefined) {
+    res.end("<head><title>404</title></head><body><h1>404 Error!</h1></body>");
+    return;
+  }
 
-	var list = await post.getPostList(parameters);
-	var blogInfo = await blog.getBlogId(parameters);
-	//없는 회사명을 입력했을 경우 404
-	if(blogInfo[0] == null || blogInfo[0] == undefined) {
-		res.end('<head><title>404</title></head><body><h1>404 Error!</h1></body>');
-		return;
-	}
+  var list = await post.getPostList(parameters);
+  var blogInfo = await blog.getBlogId(parameters);
+  //없는 회사명을 입력했을 경우 404
+  if (blogInfo[0] == null || blogInfo[0] == undefined) {
+    res.end("<head><title>404</title></head><body><h1>404 Error!</h1></body>");
+    return;
+  }
 
-	res.render("postList", {
-		"title": blogInfo[0]["title_kr"],
-		"list": list
-	});
+  res.render("postList", {
+    title: blogInfo[0]["title_kr"],
+    list: list,
+  });
 };
-//post : req.body, get : req.params
-const getPostListPost = async function(req, res) {
-	console.log(req.body["company"]);
-	console.log("[postController][getPostList2] request data : " + req.body["company"]);
-	var parameters = [req.body["company"], req.body["rowcount"]];
-	res.send({
-		data: await post.postPostList(parameters)
-	});
-}
+const getPostListPost = async function (req, res) {
+  console.log(req.body["company"]);
+  console.log(
+    "[postController][getPostList2] request data : " + req.body["company"]
+  );
+  var parameters = [req.body["company"], req.body["rowcount"]];
+  res.send({
+    data: await post.postPostList(parameters),
+  });
+};
 
 //export controller functions
 exports.list = list;
--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,23 +1,28 @@
+//post : req.body, get(url) : req.query
 var query = require("../models/query.js");
 
-const listGet = async function(req, res) {
-	console.log("[queryController][listGet]request data : " + req.param("queryText"));
-	var parameters = [req.param("queryText")];
+const listGet = async function (req, res) {
+  console.log(
+    "[queryController][listGet]request data : " + req.query["queryText"]
+  );
+  var parameters = [req.query["queryText"]];
 
-	var list = await query.listGet(parameters);
+  var list = await query.listGet(parameters);
 
-	res.render("queryResult", {
-		"queryText": parameters[0],
-		"list": list
-	});
+  res.render("queryResult", {
+    queryText: parameters[0],
+    list: list,
+  });
 };
 
-const listPost = async function(req, res) {
-	console.log("[queryController][listPost]request data : " + req.param("queryText"));
-	var parameters = [req.param("queryText"), req.param("rowcount")];
-	res.send({
-		data: await query.listPost(parameters)
-	});
+const listPost = async function (req, res) {
+  console.log(
+    "[queryController][listPost]request data : " + req.body["queryText"]
+  );
+  var parameters = [req.body["queryText"], req.body["rowcount"]];
+  res.send({
+    data: await query.listPost(parameters),
+  });
 };
 
 //export controller functions
