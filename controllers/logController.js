f930c546a8cbe8bc37ce98154a74fdba09903ea0
eurowondollaryen <sehoakasayho@naver.com>
2021-03-11 00:19:28 +0900

    replaced req.param to req.body(post)

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,30 +1,31 @@
+//post : req.body, get(url) : req.query
 var log = require("../models/log.js");
 
-const list = function(req, res) {
-	console.log("list");
+const list = function (req, res) {
+  console.log("list");
 };
 
 /*
 POST VIEW LOG
 */
-const create = async function(req, res) {
-    var clientIp = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-    console.log("[logController][create] request data : " + clientIp);
-    
-    var parameters = [req.param("post_seq"), req.param("dtl_note"), clientIp];
-    await log.create(parameters);
+const create = async function (req, res) {
+  var clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log("[logController][create] request data : " + clientIp);
+
+  var parameters = [req.body["post_seq"], req.body["dtl_note"], clientIp];
+  await log.create(parameters);
 };
 
 /*
 PAGE VISIT LOG
 */
-const visit = async function(req, res) {
-    var clientIp = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-    console.log("[logController][visit] request data : " + clientIp);
-    
-    var parameters = [clientIp, req.param("route")];
-    await log.visit(parameters);
-}
+const visit = async function (req, res) {
+  var clientIp = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log("[logController][visit] request data : " + clientIp);
+
+  var parameters = [clientIp, req.body["route"]];
+  await log.visit(parameters);
+};
 
 //export controller functions
 exports.list = list;
